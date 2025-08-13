8812e22453693afe92f4b9a9a235b455d443e9df
souleCode <souleymanetraore.2026@gmail.com>
2025-08-13 20:55:53 +0000

    App UI

---
+++ e_commerce/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:e_commerce/Provider/add_to_cart_provider.dart';
+import 'package:e_commerce/Provider/favorite_provider.dart';
+import 'package:e_commerce/pages/nav_bar_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => MultiProvider(
+    providers: [
+      // for add to cart
+      ChangeNotifierProvider(create: (_) => CartProvider()),
+      // for favorite
+      ChangeNotifierProvider(create: (_) => FavoriteProvider()),
+    ],
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.mulishTextTheme()),
+      home: const BottomNavBar(),
+    ),
+  );
+}
